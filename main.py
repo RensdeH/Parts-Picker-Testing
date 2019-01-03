37f065b0925ad21a7a30c7d824d806ca9c261c9a
Rens <rens.deheer1@gmail.com>
2019-01-03 13:57:59 +0100

    Article count test added

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ class ApiTesting(unittest.TestCase):
 	def testStoreDetails(self):
 		self.assertEqual(api.getStoreDetails()['name'],'MX5 Shop')
 
+	def testArticleCount(self):
+		self.assertTrue(api.getArtikelCount()>100)
+		self.assertEqual(len(api.getArtikels()),api.getArtikelCount())
+
+
 if __name__ == '__main__':
 	unittest.main()
